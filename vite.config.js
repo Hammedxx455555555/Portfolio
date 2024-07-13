c06174527577912ba02f3fc93b743cf00fd4b99e
Hammed Usman <147665879+Hammedxx455555555@users.noreply.github.com>
2024-07-13 21:17:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(),],
   build: {
+    outDir: 'dist' ,
     rollupOptions: {
       external: [
         'react', 
@@ -16,5 +17,5 @@ export default defineConfig({
         'react-countup'  
       ],
     },  
-  outDir: 'dist' },
+},
 })

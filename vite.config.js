b03f5b9d1589208cffcaeee076925b00a55969de
Hammed Usman <147665879+Hammedxx455555555@users.noreply.github.com>
2024-07-13 21:06:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
+  build: {
+    rollupOptions: {
+      external: [
+        'react', 
+        'react-dom', 
+        'react-router-dom',
+        'animate.css',
+        'react-icons'
+      ],
+    },
+  },
 })
